1743b66d8cffc7c789ce41288c570c3891d4f19b
Matus Tejiscak <ziman@functor.sk>
2014-03-14 15:03:49 +0000

    Exit with a proper return code in runtests.sh.

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -5,8 +5,11 @@ die() {
 	exit 1
 }
 
-rm -f *.ibc test output
+clean_up() {
+	rm -f *.ibc test output
+}
 
+clean_up
 echo "compiling..."
 idris Test.idr -p lightyear -o test || die "* could not compile tests *"
 echo "compiled OK"
@@ -15,8 +18,10 @@ timeout 5s ./test > output || die "* test failed or timed out *"
 
 if diff output expected; then
 	echo "### PASS ###"
+	clean_up
+	exit 0
 else
 	echo "### FAIL ###"
+	clean_up
+	exit 1
 fi
-
-rm -f *.ibc test output
